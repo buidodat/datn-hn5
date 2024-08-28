05da897f0173e976986a356d28b46a2f7732553c
luctc04 <luctcph37171@fpt.edu.vn>
2024-08-29 00:48:00 +0700

    cat giao dien client

---
+++ resources/views/client/layouts/partials/css.blade.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" type="text/css" href="{{ asset('theme/client/css/animate.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('theme/client/css/bootstrap.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('theme/client/css/font-awesome.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('theme/client/css/fonts.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('theme/client/css/flaticon.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('theme/client/css/owl.carousel.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('theme/client/css/owl.theme.default.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('theme/client/css/dl-menu.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('theme/client/css/nice-select.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('theme/client/css/magnific-popup.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('theme/client/css/venobox.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('theme/client/css/slick.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('theme/client/css/style3.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('theme/client/css/responsive3.css') }}" />
+<!-- favicon links -->
+<link rel="shortcut icon" type="image/png" href="{{ asset('theme/client/images/header/favicon.ico') }}" />
