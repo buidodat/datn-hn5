9d61db49d037921c7d8481f6a6c3c611698c1a24
buidodat <adudanchoi2000@gmail.com>
2024-08-28 21:58:48 +0700

    feact: layouts admin

---
+++ routes/admin.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::prefix('admin')
+    ->as('admin')
+    ->group(function (){
+
+        Route::get('/', function() {
+            return view('admin.dashboard');
+        });
+
+    });
