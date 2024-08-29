0e562be3bc0d28910d164bba20542c8d9f260839
buidodat <adudanchoi2000@gmail.com>
2024-08-29 16:00:11 +0700

    feact: contacts

--- routes/admin.php
+++ routes/admin.php
@@ -4,38 +4,24 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('admin')
-    ->as('admin.')
-    ->group(function () {
+    ->as('admin')
+    ->group(function (){
 
-        Route::get('/', function () {
+        Route::get('/', function() {
             return view('admin.dashboard');
         });
 
-        Route::prefix('posts')
-            ->as('posts.')
-            ->group(function () {
-                Route::get('/', function () {
-                    return view('admin.posts.index');
-                })->name('list');
-
-                Route::get('create', function () {
-                    return view('admin.posts.create');
-                })->name('create');
-            });
-
-
-
-
-
-        //--Start Route Giới thiệu---
-
-        Route::get('introduces', function() {
+        Route::get('posts', function() {
             return view('admin.posts.index');
         });
-        Route::get('introduces/create', function() {
+        Route::get('posts/create', function() {
             return view('admin.posts.create');
         });
 
-        //--End Route Giới thiệu---
+
+        // Route::get('posts/edit', function() {
+        //     return view('admin.posts.edit');
+        // });
+
 
     });
