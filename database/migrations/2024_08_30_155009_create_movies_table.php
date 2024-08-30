7cccb4de838b82176f522512bbc8cea9ed533957
buidodat <adudanchoi2000@gmail.com>
2024-08-31 01:17:57 +0700

    list movies

---
+++ database/migrations/2024_08_30_155009_create_movies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('category');
+            $table->string('img_thumbnail')->nullable();
+            $table->string('description')->nullable();
+            $table->string('director');
+            $table->string('cast')->nullable();
+            $table->string('rating')->nullable();
+            $table->date('release_date');
+            $table->date('end_date');
+            $table->string('trailer_url')->nullable();
+            $table->boolean('is_active')->default(1);
+            $table->boolean('is_hot')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
