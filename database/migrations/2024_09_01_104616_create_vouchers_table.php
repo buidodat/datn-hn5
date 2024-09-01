90a56b91efbc34e78614fe291ae7459d329b30db
buidodat <adudanchoi2000@gmail.com>
2024-09-01 18:13:22 +0700

    create all database table in migrate & model(not fillable)

---
+++ database/migrations/2024_09_01_104616_create_vouchers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->dateTime('start_date_time');
+            $table->dateTime('end_date_time');
+            $table->unsignedInteger('discount');
+            $table->unsignedInteger('quantity');
+            $table->unsignedTinyInteger('limit')->default(1);
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
