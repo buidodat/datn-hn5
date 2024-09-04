87b5282c2f1c0855a5e08792b1ca172479d8e4f8
an <andpph31859@fpt.edu.vn>
2024-09-05 00:12:29 +0700

    feat: reponsive trang register

--- resources/views/client/register.blade.php
+++ resources/views/client/register.blade.php
@@ -6,58 +6,44 @@
 
 @section('content')
 
-    <div class="row row-login">
-        <div class="auth-container">
-            <div class="content-register">
+    <div class="content-login-3 row" >
+        <h2 style="text-align: center">Đăng ký tài khoản</h2>
+        <div class="col-md-6">
 
-                <div class="form-input-2">
-                    <h2 class="text-login">Đăng ký</h2>
-                    <form action="#" method="POST">
+            <div class="st_profile_input float_left">
+                <label>Họ và tên</label>
+                <input type="text" >
+            </div>
+            <div class="st_profile_input float_left">
+                <label>Email</label>
+                <input type="text" >
+            </div>
+            <div class="st_profile__pass_input st_profile__pass_input_pop float_left">
+                <input type="password" placeholder="Mật khẩu">
+            </div>
+            <div class="st_profile__pass_input st_profile__pass_input_pop float_left">
+                <input type="password" placeholder="Nhập lại mật khẩu">
+            </div>
 
-                        <div class="input-container">
-                            <input type="text" name="name" placeholder="Tên người dùng">
-                        </div>
-                        <div class="input-container">
-                            <input type="email" name="email" placeholder="Email">
-                        </div>
-                        <div class="input-container">
-                            <input type="password" name="password" placeholder="Nhập mật khẩu">
-                        </div>
-                        <div class="input-container">
-                            <input type="password" name="re-password" placeholder="Nhập lại mật khẩu">
-                        </div>
-
-                        <div class="checkbox-container">
-                            <div>
-                                <input type="checkbox" id="remember">
-                                <label for="remember">Đồng ý với điều khoản của chúng tôi!</label>
-                            </div>
-                        </div>
-
-                        <div class="dangnhap">
-                            <button type="submit" >Đăng ký</button>
-                        </div>
-
-                        <div class="separator">Hoặc</div>
-
-                        <div class="social-buttons" style="display: flex; justify-content: space-around">
-                            <button>Facebook</button>
-                            <button>Google</button>
-                        </div>
-
-                        <div class="signup-link">
-                            <p>Đã có tài khoản? <a href="#">Đăng nhập</a></p>
-                        </div>
-
-                    </form>
-                </div>
-
-                <div class="logo-container-2">
-                    <img src="{{ asset('theme/client/images/auth-img.png') }}" alt="">
-                </div>
-
+            <div class="st_form_pop_login_btn float_left" style="margin-top: 15px;"><a href="page-1-7_profile_settings.html">Đăng ký</a>
+            </div>
+            <div class="st_form_pop_or_btn float_left">
+                <span>Hoặc</span>
+            </div>
+            <div class="st_form_pop_facebook_btn float_left nutfb" ><a href="#"> Đăng ký với Facebook</a>
+            </div>
+            <div class="st_form_pop_gmail_btn float_left nutgm" ><a href="#"> Đăng ký với Google</a>
+            </div>
+            <div class="st_form_pop_signin_btn float_left">
+                <h4>Đã có tài khoản? <a href="#" data-toggle="modal" data-target="#myModa3" target="_blank">Đăng nhập</a></h4>
             </div>
         </div>
+        <div class="col-md-6">
+            <div class="logo-container-2">
+                <img src="{{ asset('theme/client/images/auth-img.png') }}" alt="">
+            </div>
+        </div>
+
     </div>
 
 
