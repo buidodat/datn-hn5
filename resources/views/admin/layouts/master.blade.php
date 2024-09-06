4cc9660b7c81c854df5fb904571bb83b9b756475
luctc04 <luctcph37171@fpt.edu.vn>
2024-09-07 01:15:46 +0700

    responsive contact

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -33,6 +33,7 @@
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 
+    @yield('styles')
 
 
 </head>
--- resources/views/client/layouts/master.blade.php
+++ resources/views/client/layouts/master.blade.php
@@ -12,9 +12,14 @@
     <meta name="keywords" content="Movie Pro" />
     <meta name="author" content="" />
     <meta name="MobileOptimized" content="320" />
+    
+    @yield('style-libs')
+
     <!--Template style -->
     @include('client.layouts.partials.css')
     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" /> --}}
+
+    @yield('styles')
 
 </head>
 
