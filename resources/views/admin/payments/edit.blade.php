92dfa4cbbe3c36bbde6dae50e6b3d9e0bde718d7
hadachieu <hadachieu140504@gmail.com>
2024-09-08 22:53:46 +0700

    CRUD validate đầy đủ payments và sửa lại update type_seats

--- resources/views/admin/payments/edit.blade.php
+++ resources/views/admin/payments/edit.blade.php
@@ -1,22 +1,23 @@
 @extends('admin.layouts.master')
 
 @section('title')
-    Thêm mới phương thức thanh toán
+    Cập nhật phương thức thanh toán
 @endsection
 
 @section('content')
-<form action="{{route('admin.payments.store')}}" method="post" enctype="multipart/form-data">
+<form action="{{route('admin.payments.update', $payment)}}" method="post" enctype="multipart/form-data">
+    @method('PUT')
     @csrf
 
     <div class="row">
         <div class="col-12">
             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                <h4 class="mb-sm-0">Thêm mới Thanh Toán</h4>
+                <h4 class="mb-sm-0">Cập nhật Thanh Toán</h4>
 
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
                         <li class="breadcrumb-item"><a href="javascript: void(0);">Forms</a></li>
-                        <li class="breadcrumb-item active">Thêm mới</li>
+                        <li class="breadcrumb-item active">Cập nhật</li>
                     </ol>
                 </div>
 
@@ -38,6 +39,19 @@
                 <div class="card-header align-items-center d-flex">
                     <h4 class="card-title mb-0 flex-grow-1">Thông tin phương thức thanh toán</h4>
                 </div><!-- end card header -->
+
+                @if (session()->has('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+                @endif
+
+                @if (session()->has('error'))
+                    <div class="alert alert-success">
+                        {{ session('error') }}
+                    </div>
+                @endif
+
                 <div class="card-body">
                     <div class="live-preview">
                         <div class="row gy-4">
@@ -46,7 +60,8 @@
                                 <div class="row">
                                     <div class="col-md-4 mb-3">
                                         <label for="name" class="form-label">Tên phương thức thanh toán:</label>
-                                        <input type="text" class="form-control" id="name" name="name" placeholder="Nhập tên phương thức thanh toán">
+                                        <input type="text" class="form-control" id="name" name="name"
+                                        value="{{$payment->name}}" placeholder="Nhập tên phương thức thanh toán">
                                         @error("name")
                                             <span class="text-danger">{{ $message }}</span>
                                         @enderror
@@ -56,7 +71,8 @@
                                 <div class="row">
                                     <div class="col-md-4 mb-3">
                                         <label for="description" class="form-label">Mô tả:</label>
-                                        <textarea class="form-control" rows="3" name="description" placeholder="Nhập mô tả thanh toán"></textarea>
+                                        <textarea class="form-control" rows="3" name="description" 
+                                        placeholder="Nhập mô tả thanh toán">{{ $payment->description }}</textarea>
                                         @error('description')
                                             <span class="text-danger">{{ $message }}</span>
                                         @enderror
@@ -75,7 +91,7 @@
             <div class="card">
                 <div class="card-header align-items-center d-flex">
                     <a href="{{ route('admin.payments.index') }}" class="btn btn-info">Danh sách</a>
-                    <button type="submit" class="btn btn-primary mx-1">Thêm mới</button>
+                    <button type="submit" class="btn btn-primary mx-1">Cập nhật</button>
                 </div>
             </div>
         </div>
