2d9fd253b38d3a243c3225693cc28e533d3e966e
luctc04 <luctcph37171@fpt.edu.vn>
2024-09-20 15:23:19 +0700

    xóa mấy dòng thừa

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -739,34 +739,3 @@
         });
     </script>
 @endsection
-
-
-
-{{-- // Lấy số lượng rạp và phòng đã có
-        $roomCount = DB::table('rooms')->count();
-
-        // Tạo dữ liệu cho bảng seats
-        for ($room_id = 1; $room_id <= $roomCount; $room_id++) {
-            for ($x = 1; $x <= 10; $x++) { // Tạo 10 hàng ghế (trục x)
-                for ($y = 'A'; $y <= 'J'; $y++) { // Tạo 10 cột ghế (trục y)
-                    
-                    // Xác định loại ghế dựa trên hàng (y)
-                    if (in_array($y, ['A', 'B', 'C', 'D', 'E'])) {
-                        $type_seat_id = 1; // Ghế thường
-                    } else {
-                        $type_seat_id = 2; // Ghế VIP
-                    }
-
-                    DB::table('seats')->insert([
-                        'room_id' => $room_id,
-                        'type_seat_id' => $type_seat_id,
-                        'coordinates_x' => $x,
-                        'coordinates_y' => $y,
-                        'name' => $y . $x,
-                        'is_active' => 1, 
-                        'created_at' => now(),
-                        'updated_at' => now(),
-                    ]);
-                }
-            }
-        } --}}
