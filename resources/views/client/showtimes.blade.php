cc1c380e91ce3606398b029144b2ee47cae90196
luctc04 <luctcph37171@fpt.edu.vn>
2024-09-29 21:10:26 +0700

    lọc các suất chiếu hợp lệ

--- resources/views/client/showtimes.blade.php
+++ resources/views/client/showtimes.blade.php
@@ -21,8 +21,7 @@
                     </div>
                 </div>
 
-
-                @foreach ($dates as $date)
+                {{-- @foreach ($dates as $date)
                     <div class="row" data-day="{{ $date['day_id'] }}" style="{{ $loop->first ? '' : 'display:none;' }}">
                         <!-- Chỉ hiển thị hàng đầu tiên -->
                         @foreach ($date['showtimes'] as $movieId => $showtimes)
@@ -40,7 +39,7 @@
                                     <!-- Lịch chiếu phim -->
                                     <div class="showtime-section">
                                         @foreach ($showtimes->groupBy('format') as $format => $times)
-                                            <div style="margin-bottom: 20px">
+                                            <div class="showtime-bot">
                                                 <h4 class="showtime-title">{{ $format }}</h4>
                                                 <div class="showtime-list">
                                                     @foreach ($times as $showtime)
@@ -59,10 +58,75 @@
                             <hr>
                         @endforeach
                     </div>
+                @endforeach --}}
+
+                @foreach ($dates as $date)
+                    <div class="row" data-day="{{ $date['day_id'] }}" style="{{ $loop->first ? '' : 'display:none;' }}">
+                        <!-- Chỉ hiển thị hàng đầu tiên -->
+                        @foreach ($date['showtimes'] as $movieId => $showtimes)
+                            @php
+                                // Lọc các suất chiếu hợp lệ, chỉ những suất chiếu sau thời gian hiện tại
+                                $validShowtimes = $showtimes->filter(function ($showtime) {
+                                    return \Carbon\Carbon::parse($showtime->start_time, 'Asia/Ho_Chi_Minh')->isAfter(
+                                        \Carbon\Carbon::now('Asia/Ho_Chi_Minh'),
+                                    );
+                                });
+                            @endphp
+
+                            @if ($validShowtimes->isNotEmpty() && $validShowtimes->first() && $validShowtimes->first()->movie)
+                                <!-- Chỉ hiển thị nếu có suất chiếu hợp lệ -->
+                                <div class="col-xl-8 col-lg-12 col-md-12 col-sm-12 col-8" style="margin-bottom: 30px">
+                                    <div class="col-md-4 image-movie-detail">
+                                        <img src="{{ $validShowtimes->first()->movie->img_thumbnail }}"
+                                            class="movie-poster">
+                                    </div>
+                                    <div class="movie-detail-content">
+                                        <h1 class="movie-title">{{ $validShowtimes->first()->movie->name }}</h1>
+                                        <ul class="movie-info">
+                                            <li><strong>Thể loại:</strong> {{ $validShowtimes->first()->movie->category }}
+                                            </li>
+                                            <li><strong>Thời lượng:</strong> {{ $validShowtimes->first()->movie->duration }}
+                                                phút</li>
+                                        </ul>
+                                        <!-- Lịch chiếu phim -->
+                                        <div class="showtime-section">
+                                            @foreach ($validShowtimes->groupBy('format') as $format => $times)
+                                                @php
+                                                    // Lọc các suất chiếu hợp lệ trong từng format
+                                                    $validTimes = $times->filter(function ($showtime) {
+                                                        return \Carbon\Carbon::parse(
+                                                            $showtime->start_time,
+                                                            'Asia/Ho_Chi_Minh',
+                                                        )->isAfter(\Carbon\Carbon::now('Asia/Ho_Chi_Minh'));
+                                                    });
+                                                @endphp
+
+                                                @if ($validTimes->isNotEmpty())
+                                                    <!-- Chỉ hiển thị format nếu có suất chiếu hợp lệ -->
+                                                    <div class="showtime-bot">
+                                                        <h4 class="showtime-title">{{ $format }}</h4>
+                                                        <div class="showtime-list">
+                                                            @foreach ($validTimes as $showtime)
+                                                                <a href="{{ route('choose-seat', $showtime->id) }}"
+                                                                    class="showtime-btn">
+                                                                    {{ \Carbon\Carbon::parse($showtime->start_time, 'Asia/Ho_Chi_Minh')->format('H:i') }}
+                                                                </a>
+                                                            @endforeach
+                                                        </div>
+                                                    </div>
+                                                @endif
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr>
+                            @endif
+                        @endforeach
+                    </div>
                 @endforeach
             @else
-                <div style="margin: 130px 0px 300px 0px;">
-                    <p style="text-align: center; font-size: 30px;">Hiện tại rạp đang không có suất chiếu nào!</p>
+                <div class="div-1">
+                    <p class="p-1">Hiện tại rạp đang không có suất chiếu nào!</p>
                 </div>
             @endif
         </div>
