42cbd281c0e82980c0e0203e106c28df3cc3da5a
Moderator <andpph31859@fpt.edu.vn>
2024-10-21 17:34:40 +0700

    validate: voucher

--- app/Http/Controllers/Admin/VoucherController.php
+++ app/Http/Controllers/Admin/VoucherController.php
@@ -48,7 +48,7 @@ class VoucherController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreVoucherRequest $request)
     {
         $data = $request->all();
 
