4f9ae69bf1187a4f959b9cc0892b27beb4f951c3
buidodat <adudanchoi2000@gmail.com>
2024-11-13 18:46:42 +0700

    Fake dữ liệu phim siêu đẹp & Tên phim thể loại phim việt hóa

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -54,16 +54,10 @@ class DatabaseSeeder extends Seeder
         ]);
 
         //20 bản ghi movie và 40 bản ghi movie_version
-        $img_thumbnails = [
-            'https://files.betacorp.vn/media%2fimages%2f2024%2f08%2f27%2f400x633%2D13%2D093512%2D270824%2D67.jpg',
-            'https://files.betacorp.vn/media%2fimages%2f2024%2f08%2f08%2fscreenshot%2D2024%2D08%2D08%2D151702%2D151742%2D080824%2D61.png',
-            'https://files.betacorp.vn/media%2fimages%2f2024%2f08%2f16%2f400x633%2D5%2D161700%2D160824%2D33.jpg',
-            'https://files.betacorp.vn/media%2fimages%2f2024%2f08%2f21%2fposter%2Dhellboy%2D105547%2D210824%2D98.jpg',
-        ];
         $url_youtubes = [
             'VmJ4oB3Xguo',
             'XuX2HKeMkVw',
-            'SGg9DxLFCtc'
+            'SGg9DxLFCtc','m6MF1MqsDhc','dNwuFYhwTAk','4oxoPMxBO6s','b1Yqng0uSWM','IK-eb2AbKQ','Tx5JuN-5n8U','kMjlJkmt5nk','gTo9JwsmjT4','4rgYUipGJNo'
         ];
         $booleans = [
             true,
@@ -77,16 +71,59 @@ class DatabaseSeeder extends Seeder
         ];
 
         $ratings = ['P',  'T13', 'T16', 'T18', 'K'];
+        $categories = [
+            "Hành động, kịch tính",
+            "Phiêu lưu, khám phá",
+            "Kinh dị",
+            "Khoa học viễn tưởng",
+            "Tình cảm",
+            "Hài hước",
+            "Kịch, Hồi Hộp",
+            "Hoạt hình",
+            "Tâm lý",
+            "Âm nhạc, phiêu lưu",
+        ];
+        $movieNames =  [
+            "Moana 2: Hành Trình Của Moana",
+            "Thợ Săn Thủ Lĩnh",
+            "Nhím Sonic III",
+            "Spring Garden: Ai Oán Trong Vườn Xuân",
+            "Tee Yod: Quỷ Ăn Tạng II",
+            "Vùng Đất Bị Nguyền Rủa",
+            "Gladiator: Võ Sĩ Giác Đấu II",
+            "Elli và Bí Ẩn Chiếc Tàu Ma",
+            "Sắc Màu Của Hạnh Phúc",
+            "OZI: Phi Vụ Rừng Xanh",
+            "Tee Yod: Quỷ Ăn Tạng",
+            "Venom: Kèo Cuối",
+            "Ngày Xưa Có Một Chuyện Tình",
+            "Cười Xuyên Biên Giới",
+            "Thiên Đường Quả Báo",
+            "Cu Li Không Bao Giờ Khóc",
+            "RED ONE: Mật mã đỏ",
+            "Vây Hãm Tại Đài Bắc",
+            'Học Viện Anh Hùng',
+            "Linh Miêu",
+            'Công Tử Bạc Liêu',
+            "CAPTAIN AMERICA: BRAVE NEW WORLD",
+            "Địa Đạo: Mặt Trời Trong Bóng Tối",
+            "Thám Tử Kiên: Kỳ Án Không Đầu",
+            'Mufasa: Vua Sư Tử'
+        ];
 
-        for ($i = 0; $i < 35; $i++) {
+
+        for ($i = 0; $i < 25; $i++) {
             $releaseDate = fake()->dateTimeBetween(now()->subMonths(5), now()->addMonths(2));
             $endDate = fake()->dateTimeBetween($releaseDate, now()->addMonths(5));
             $rating = $ratings[array_rand($ratings)];
+            $x = ($i % 21) + 1;
+
+            $img = "images/movies/". $x . ".png";
             $movie = DB::table('movies')->insertGetId([
-                'name' => $name = fake()->unique()->name(),
-                'slug' => Str::slug($name),
-                'category' => fake()->word,
-                'img_thumbnail' => $img_thumbnails[rand(0, 3)],
+                'name' => $movieNames[$i],
+                'slug' => Str::slug($movieNames[$i]),
+                'category' =>  $categories[array_rand($categories)],
+                'img_thumbnail' => asset($img),
                 'description' => Str::limit(fake()->paragraph, 250),
                 'director' => fake()->name,
                 'cast' => fake()->name(),
--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -44,80 +44,47 @@
                                 <div class="row" id="movie-list1">
                                     @foreach ($moviesUpcoming as $movie)
                                         <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 prs_upcom_slide_first">
-                                            <div class="prs_upcom_movie_box_wrapper">
-                                                <div class="movie_box_wrapper">
-                                                    <div class="movie_img_box">
-                                                        @if ($movie->is_hot == '1')
-                                                            <img class="is_hot" src="{{ asset('theme/client/images/hot.png') }}"
-                                                                alt="">
-                                                        @endif
-                                                        @php
-                                                            $imageTag = App\Models\Movie::getImageTagRating($movie->rating);
-                                                        @endphp
-                                                        @if ($imageTag)
-                                                            <img class="tag-rating" src="{{ $imageTag }}" alt="">
-                                                        @endif
-                                                        @php
-                                                            $url = $movie->img_thumbnail;
-
-                                                            if (!\Str::contains($url, 'http')) {
-                                                                $url = Storage::url($url);
-                                                            }
-
-                                                        @endphp
-
-                                                        <div class='img_thumbnail_movie'>
-                                                            <img src="{{ $url }}" alt="movie_img" />
-                                                        </div>
-                                                        <div class='movie_img_trailer'>
-                                                            <div class='animation-icon open-trailer-btn'
-                                                                data-trailer-url="https://www.youtube.com/embed/{{ $movie->trailer_url }}"
-                                                                data-movie-name="{{ $movie->name }}">
-                                                                <img src="{{ asset('theme/client/images/index_III/icon.png') }}"
-                                                                    alt="img">
-                                                            </div>
-                                                        </div>
-                                                    </div>
-
-
-                                                    <div class="content-movie">
-                                                        <h3 class="movie-name-home">
-                                                            <a
-                                                                href="movies/{{ $movie->slug }}">{{ Str::limit($movie->name, 19) }}</a>
-                                                        </h3>
-                                                        <p><span class='text-bold'>Thể loại:</span> {{ $movie->category }}</p>
-                                                        <p><span class='text-bold'>Thời lượng:</span> {{ $movie->duration }}
-                                                            phút </p>
-
-                                                    </div>
-
-
+                                            <div class="movie_box_wrapper">
+                                                <div class="movie_img_box">
+                                                    @if ($movie->is_hot == '1')
+                                                        <img class="is_hot" src="{{ asset('theme/client/images/hot.png') }}"
+                                                            alt="">
+                                                    @endif
                                                     @php
-                                                        // Kiểm tra có suất chiếu trong 7 ngày tới tại cinema_id
-                                                        $hasShowtimeInNextWeek = $movie
-                                                            ->showtimes()
-                                                            ->where('cinema_id', session('cinema_id')) // Kiểm tra theo cinema_id
-                                                            ->whereBetween('start_time', [$currentNow, $endDate])
-                                                            ->exists();
+                                                        $imageTag = App\Models\Movie::getImageTagRating($movie->rating);
+                                                    @endphp
+                                                    @if ($imageTag)
+                                                        <img class="tag-rating" src="{{ $imageTag }}" alt="">
+                                                    @endif
+                                                    @php
+                                                        $url = $movie->img_thumbnail;
+
+                                                        if (!\Str::contains($url, 'http')) {
+                                                            $url = Storage::url($url);
+                                                        }
+
                                                     @endphp
 
-                                                    <div class='buy-ticket-movie'>
-                                                        @if ($hasShowtimeInNextWeek)
-                                                            <button onclick="openModalMovieScrening({{ $movie->id }})"
-                                                                class="buy-ticket-btn">MUA VÉ</button>
-                                                        @endif
+                                                    <div class='img_thumbnail_movie'>
+                                                        <img src="{{ $url }}" alt="movie_img" />
                                                     </div>
-
-
+                                                    <div class='movie_img_trailer'>
+                                                        <div class='animation-icon open-trailer-btn'
+                                                            data-trailer-url="https://www.youtube.com/embed/{{ $movie->trailer_url }}"
+                                                            data-movie-name="{{ $movie->name }}">
+                                                            <img src="{{ asset('theme/client/images/index_III/icon.png') }}"
+                                                                alt="img">
+                                                        </div>
+                                                    </div>
                                                 </div>
 
 
                                                 <div class="content-movie">
                                                     <h3 class="movie-name-home">
                                                         <a
-                                                            href="movies/{{ $movie->slug }}">{{ Str::limit($movie->name, 19) }}</a>
+                                                            href="movies/{{ $movie->slug }}">{{ Str::limit($movie->name, 28) }}</a>
                                                     </h3>
-                                                    <p><span class='text-bold'>Thể loại:</span> {{ $movie->category }} </p>
+                                                    <p><span class='text-bold'>Thể loại:</span> {{ $movie->category }}   </p>
                                                     <p><span class='text-bold'>Thời lượng:</span> {{ $movie->duration }}
                                                         phút </p>
                                                     <p><span class='text-bold'>Ngày khởi chiếu:</span>
@@ -147,6 +114,7 @@
 
 
                                             </div>
+
                                         </div>
                                     @endforeach
                                 </div>
@@ -215,9 +183,9 @@
                                                 <div class="content-movie">
                                                     <h3 class="movie-name-home">
                                                         <a
-                                                            href="movies/{{ $movie->slug }}">{{ Str::limit($movie->name, 19) }}</a>
+                                                            href="movies/{{ $movie->slug }}">{{ Str::limit($movie->name, 28) }}</a>
                                                     </h3>
-                                                    <p><span class='text-bold'>Thể loại:</span> {{ $movie->category }}</p>
+                                                    <p><span class='text-bold'>Thể loại:</span> {{ $movie->category }} </p>
                                                     <p><span class='text-bold'>Thời lượng:</span> {{ $movie->duration }}
                                                         phút </p>
 
@@ -309,7 +277,7 @@
                                                 <div class="content-movie">
                                                     <h3 class="movie-name-home">
                                                         <a
-                                                            href="movies/{{ $movie->slug }}">{{ Str::limit($movie->name, 19) }}</a>
+                                                            href="movies/{{ $movie->slug }}">{{ Str::limit($movie->name, 28) }}</a>
                                                     </h3>
                                                     <p><span class='text-bold'>Thể loại:</span> {{ $movie->category }}</p>
                                                     <p><span class='text-bold'>Thời lượng:</span> {{ $movie->duration }}
@@ -395,7 +363,7 @@
                                             </div>
                                         </div>
                                         <div class="prs_feature_img_cont">
-                                            <h2>{!! Str::limit($postItem->title, 30) !!}</h2>
+                                            <h2>{!! Str::limit($postItem->title, 28) !!}</h2>
                                             <br>
                                             <div class="prs_ft_small_cont_center">
                                                 {!! Str::limit($postItem->description, 70) !!}
